f63c02296ebcb85e938d9bd6c36d22a2a2d11889
Steve Winton <stevewinton@gmail.com>
2023-06-01 11:37:15 -0500

    Append new data

--- index.js
+++ index.js
@@ -3,29 +3,18 @@
 const { google } = require('googleapis');
 
 const authorize = require('./lib/auth');
+const { append } = require('./lib/sheets');
 
 async function main() {
   try {
     const auth = await authorize();
     const sheets = google.sheets({ version: 'v4', auth });
+    const unixTimestamp = Math.floor(Date.now() / 1000);
+    const response = await append(sheets, unixTimestamp, 1, 1);
 
-    const range = 'Sheet1!A1:B2'; // Specify the range to update
-    const values = [['New Value A1', 'New Value B1'], ['New Value A2', 'New Value B2']]; // Specify the new values
-
-    const resource = {
-      values,
-    };
-
-    const response = await sheets.spreadsheets.values.update({
-      spreadsheetId: process.env.SPREADSHEET_ID,
-      range,
-      valueInputOption: 'RAW',
-      resource,
-    });
-
-    console.log(`${response.data.updatedCells} cells updated.`);
+    console.log(`${response.data.updates.updatedCells} cells updated.`);
   } catch (error) {
-    console.error('The API returned an error:', error.message);
+    console.error(`Error: ${error.message}`);
   }
 }
 
