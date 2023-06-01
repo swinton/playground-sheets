5cee953c6f4e14eefc7a9efd4aadcfd4547eee50
Steve Winton <stevewinton@gmail.com>
2023-06-01 11:51:47 -0500

    Append new row every 2 seconds

--- index.js
+++ index.js
@@ -5,14 +5,23 @@ const { google } = require('googleapis');
 const authorize = require('./lib/auth');
 const { append } = require('./lib/sheets');
 
+async function appendValues(sheets, ...values) {
+  const unixTimestamp = Math.floor(Date.now() / 1000);
+  const response = await append(sheets, unixTimestamp, ...values);
+  console.log(`${response.data.updates.updatedCells} cells updated.`);
+}
+
 async function main() {
   try {
     const auth = await authorize();
     const sheets = google.sheets({ version: 'v4', auth });
-    const unixTimestamp = Math.floor(Date.now() / 1000);
-    const response = await append(sheets, unixTimestamp, 1, 1);
-
-    console.log(`${response.data.updates.updatedCells} cells updated.`);
+    let counters = [5000, 5000];
+    const interval = setInterval(async () => {
+      await appendValues(sheets, ...counters);
+      counters = counters.map((value, index) => {
+        return index % 2 === 0 ? value - 200 : value - 10;
+      });
+    }, 2000);
   } catch (error) {
     console.error(`Error: ${error.message}`);
   }
